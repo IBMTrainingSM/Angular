9d027e392b55b3f611f6c56a01bd29460ddca396
unknown <ibmjavanh18@iiht.tech>
2021-04-16 11:44:37 +0530

    Angular File

---
+++ src/app/userform/userform.component.ts
@@ -0,0 +1,32 @@
+
+import { Component, OnInit } from '@angular/core';
+import { User } from '../User';
+import { UserService } from '../user-service';
+
+@Component({
+  selector: 'app-userform',
+  templateUrl: './userform.component.html',
+  styleUrls: ['./userform.component.css']
+})
+export class UserformComponent implements OnInit {  //controller
+  title:string = 'Userform';
+  user:User=new User(); //model -stores all form data
+  userArray:User[]=[];
+  constructor(private userService:UserService) { }
+  save(){
+    const promise = this.userService.save(this.user);
+    promise.subscribe(response=> {
+      console.log(response);
+      alert('user added..')
+      this.userArray.push(Object.assign({}, this.user));
+    },
+    error=> {
+      console.log(error);
+      alert(error.statusText)
+    })
+    // this.user.firstname = 'John';
+
+  }
+  ngOnInit(): void {
+  }
+}
